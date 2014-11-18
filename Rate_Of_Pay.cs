5a3e5b8907ebf3eb22a6fbb0b29cb1d477bcf232
Stephen Kohlmann <stephen.kohlmann@gmail.com>
2014-11-18 22:17:00 +0000

    Calculates rate of pay

---
+++ Rate_Of_Pay.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_Lab_1_Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user for hourly pay
+            Console.WriteLine("What is your hourly pay");
+            //Read in and store
+            decimal rate = Convert.ToDecimal(Console.ReadLine());
+            //Ask for hours worked
+            Console.WriteLine("Enter hours worked");
+            //Read in and store
+            decimal hoursWorked = Convert.ToDecimal(Console.ReadLine());
+            //Calculate
+
+            //are hours greater than 40
+            if (hoursWorked > 40m)
+            {
+                //if they are by how many
+                decimal overTimehours = hoursWorked - 40;
+                //Overtime rate
+                decimal overTimerate = rate * 1.5m;
+                //work out basic
+                decimal basic = 40 * rate;
+                //work out overtime
+                decimal overTimetotal = overTimehours * overTimerate;
+                //work out total
+                decimal total = basic + overTimetotal;
+
+                Console.WriteLine("Your pay is {0:f)", total);
+
+            }
+
+            //if not simple calculation
+            else
+            {
+                Console.WriteLine("Your rate of pay is {0:f}", rate * hoursWorked);
+
+
+            }
+
+            //Readline
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+
+}

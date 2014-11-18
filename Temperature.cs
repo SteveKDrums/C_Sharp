4907be760320a2d7feb8c4ee42dfc8f5651e1340
Stephen Kohlmann <stephen.kohlmann@gmail.com>
2014-11-18 22:17:45 +0000

    Calculates temperature

---
+++ Temperature.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Lab2Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the temperature");
+            int temperature = Convert.ToInt32 (Console.ReadLine());
+
+            if (temperature<15)
+            Console.WriteLine("Put on your coat");
+
+            Console.ReadLine();
+
+        }
+    }
+}

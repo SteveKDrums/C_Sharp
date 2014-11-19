38a4a9735cd32114fdbf7750d863d158e4851733
Stephen Kohlmann <stephen.kohlmann@gmail.com>
2014-11-19 15:20:09 +0000

    Simple Bank program

---
+++ Bank_If_Else.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk3_Lab1_Q9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+           decimal balance = rand.Next(100);
+           
+            //Enter How much to withdraw
+            Console.WriteLine("How much would you like to withdraw?");
+           
+            //declare decimal
+            decimal withDraw = Convert.ToDecimal(Console.ReadLine());
+           
+            //Display balance of account random
+
+            Console.WriteLine(balance);
+           
+  
+
+            //Program decide if sufficent funds available and Output Approved or declined
+            if (withDraw > balance)
+                Console.WriteLine("Declined");
+            else
+                Console.WriteLine("Approved");
+
+            
+
+            //Pause program
+            Console.ReadLine();
+        }
+    }
+}

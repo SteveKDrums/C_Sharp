1f13cf856efec479da029f1581434c7c02b9b617
Stephen Kohlmann <stephen.kohlmann@gmail.com>
2014-11-20 12:44:27 +0000

    Using the If statement

---
+++ Simple_If.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk3_Lab1_Ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Enter Age
+            Console.WriteLine("Enter your age");
+
+            //Declare Age as Int and read to console
+            int age = Convert.ToInt32 (Console.ReadLine());
+           
+            //Calculate
+            //If < 16 || or //Or > 65
+            if (age < 16 || age > 65)
+                
+                //Output
+                Console.WriteLine("You are entitled to a discount");
+            
+            //Pause Program
+            Console.ReadLine();
+        }
+    }
+}

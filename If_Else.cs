c101e458e9a87903c8d975ee8f6a3f5aed4fe679
Stephen Kohlmann <stephen.kohlmann@gmail.com>
2014-11-20 12:42:38 +0000

    Simple use of If and Else

---
+++ If_Else.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk3_Lab1_Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask for name
+            Console.WriteLine("What is your name?");
+            //Read name
+            string name = Console.ReadLine();
+            //Calculate
+            if (name != "Keith")
+            //Output
+                Console.WriteLine("You are welcome");
+            else
+                Console.WriteLine("You cannot come into the club");
+            
+
+            //Pause
+            Console.ReadLine();
+        }
+    }
+}

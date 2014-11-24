cd88f5da02bbc751a4bcca2af6f361f6589c38d3
Stephen Kohlmann <stephen.kohlmann@gmail.com>
2014-11-24 19:53:56 +0000

    Using a while loop to find the averagre

---
+++ While_Loop_For_Average.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk4_Lab1_Q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int total = 0;
+            int counter = 1;
+
+            while (counter <= 3)
+            {
+                Console.WriteLine("Enter number",num1);
+                Console.ReadLine();
+                counter++;
+            
+            }
+            total = num1;
+            int average = (3 / num1 );
+
+            Console.WriteLine("The average number is {0}", average);
+            Console.ReadLine();
+
+
+        }
+
+    }
+}

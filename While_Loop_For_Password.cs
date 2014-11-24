f2cd9b76c54a099961a851ddab8810154c08de81
Stephen Kohlmann <stephen.kohlmann@gmail.com>
2014-11-24 19:55:11 +0000

    Using a while loop to test if user entry matches a predefined password

---
+++ While_Loop_For_Password.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk4_Lab1_Q10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password;
+            string x = "goofy";
+            
+            Console.WriteLine("Please enter password");
+            password = Console.ReadLine();
+            while (password != x)
+            {
+                Console.WriteLine("Incorrect password, please try again");
+                password = Console.ReadLine();
+            
+            }
+
+            Console.WriteLine("Correct Password");
+                Console.ReadLine();
+        }
+    }
+}

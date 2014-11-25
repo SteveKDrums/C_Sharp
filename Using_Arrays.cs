cd29e672774bcdaeb0a553b13ad7c53677de4255
Stephen Kohlmann <stephen.kohlmann@gmail.com>
2014-11-25 23:36:09 +0000

    Simple use of an array

---
+++ Using_Arrays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk6_Lab1_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Stephen", "Sinead", "Scott" };
+
+            for (int i = 0; i < 3; i++) 
+
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}

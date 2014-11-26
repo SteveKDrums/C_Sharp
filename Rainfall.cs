7dfd4c1d574bce14783b11afdfe413f7443d88db
Stephen Kohlmann <stephen.kohlmann@gmail.com>
2014-11-26 23:48:29 +0000

    Arrays and loops to work out average rainfall for a week

---
+++ Rainfall.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk6_Lab1_Q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] rainFall = { 3.9, 0.7, 9.8, 0.1, 6.5, 3.2, 1 };
+            string[] weekDays = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            double total = 0;
+            for (int i = 0; i < rainFall.Length; i++)
+            {
+                Console.WriteLine("{0} : {1}", weekDays[i], rainFall[i]);
+
+                total = total + rainFall[i];
+            }// end of for loop
+
+
+            //output numbers
+            Console.WriteLine("Total is {0}", total);
+            Console.ReadLine();
+        }
+    }
+}
